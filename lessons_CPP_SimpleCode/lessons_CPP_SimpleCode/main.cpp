75c609f4801441cc52a02101d6fb548399f3ac64
lianino <lianino@mail.ru>
2019-02-26 21:46:21 +0700

    Работа с auto,unique,shared указателями

--- lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
+++ lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 #include<string>
+#include <memory>
 using namespace std;
 
 //smart pointer
+// два указателя ссылаются на одну и туже память
+//auto_ptr устарел
+//unique_ptr уникальный
+//shared_ptr общий
+
 
 template<typename T>
 class SmartPointer
@@ -36,10 +42,20 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 	
-	SmartPointer<int> pointer = new int(5);
-	cout << *pointer << endl;
+	/*auto_ptr<int> ap1(new int(5));
+	auto_ptr<int> ap2(ap1);*/
+
+	/*unique_ptr<int> p1(new int(5));
+	unique_ptr<int> p2;
+	p2 = move(p1);
+	p1.swap(p2);
+	int *p = p1.get();
+	//p1.release(); // clear pointer
+	p1.reset();// clear pointer amd memory*/
+
+	shared_ptr<int> p1(new int(5));
+	shared_ptr<int> p2(p1);
 
 	system("pause");
-
 	return 0;
 }
