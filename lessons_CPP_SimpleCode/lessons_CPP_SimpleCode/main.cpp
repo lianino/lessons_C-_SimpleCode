3b76c63d353da0de450534367ac6794672b23c3b
lianino <lianino@mail.ru>
2019-02-28 21:43:53 +0700

    СОздание класса односвязного списка clas list и элемента списка class Node

--- lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
+++ lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
@@ -3,58 +3,46 @@
 #include <memory>
 using namespace std;
 
-//smart pointer
-// два указателя ссылаются на одну и туже память
-//auto_ptr устарел
-//unique_ptr уникальный
-//shared_ptr общий
-
+//односвязный список
 
 template<typename T>
-class SmartPointer
+class List
 {
 public:
-	SmartPointer(T *ptr)
-	{
-		this->ptr = ptr;
-		cout << "Constructor SmartPointer" << endl;
-	}
-
-	~SmartPointer()
-	{
-		delete ptr;
-		cout << "Destructor SmartPointer" << endl;
-	}
-
-	T& operator*()
-	{
-		return *ptr;
-	}
-
+	List();
+	~List();
 
 private:
-	T *ptr;
-
+	template<typename T>
+	class Node
+	{
+	public:
+		Node *pNext;
+		T data;
+		Node(T data = T(), Node *pNext = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+	Node<T> head;
 };
+
+template<typename T>
+List<T>::List()
+{
+}
+
+template<typename T>
+List<T>::~List()
+{
+}
 
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 	
-	/*auto_ptr<int> ap1(new int(5));
-	auto_ptr<int> ap2(ap1);*/
-
-	/*unique_ptr<int> p1(new int(5));
-	unique_ptr<int> p2;
-	p2 = move(p1);
-	p1.swap(p2);
-	int *p = p1.get();
-	//p1.release(); // clear pointer
-	p1.reset();// clear pointer amd memory*/
-
-	shared_ptr<int> p1(new int(5));
-	shared_ptr<int> p2(p1);
 
 	system("pause");
 	return 0;
