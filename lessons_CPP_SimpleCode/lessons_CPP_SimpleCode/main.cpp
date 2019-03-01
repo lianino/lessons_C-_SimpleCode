32aea7d2fe72bd3f8a12f940b3b146311ca86bd2
lianino <lianino@mail.ru>
2019-03-01 15:00:06 +0700

    Реализация метода push_back, заполнение и вывод односвязного списка

--- lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
+++ lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<string>
-#include <memory>
 using namespace std;
 
 //односвязный список
@@ -11,6 +9,11 @@ class List
 public:
 	List();
 	~List();
+
+	void push_back(T data);
+	int GetSize() { return Size; };
+
+	T& operator[](const int index);
 
 private:
 	template<typename T>
@@ -25,12 +28,15 @@ private:
 			this->pNext = pNext;
 		}
 	};
-	Node<T> head;
+	int Size;
+	Node<T> *head;
 };
 
 template<typename T>
 List<T>::List()
 {
+	Size = 0;
+	head = nullptr;
 }
 
 template<typename T>
@@ -38,11 +44,66 @@ List<T>::~List()
 {
 }
 
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+		Node<T> *current = this->head;
+		while(current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+	Size++;
+}
+
+template<typename T>
+T & List<T>::operator[](const int index)
+{
+	int counter = 0;
+	Node<T> *current = this->head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+		{
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
+
+	List<int> lst;
+	/*lst.push_back(5);
+	lst.push_back(10);
+	lst.push_back(22);*/
 	
+	//cout << lst.GetSize() << endl;
+	//cout << lst[2] << endl;
+
+	int NumbersCount;
+	cin >> NumbersCount;
+	for (int i = 0; i < NumbersCount; i++)
+	{
+		lst.push_back(rand() % 10);
+	}
+
+	for (int i = 0; i < lst.GetSize(); i++)
+	{
+		cout << lst[i] << endl;
+	}
 
 	system("pause");
 	return 0;
