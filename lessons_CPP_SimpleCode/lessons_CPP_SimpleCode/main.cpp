a2a520ba7b103a8e940a915ff6a4efffd4be96be
lianino <lianino@mail.ru>
2019-03-02 19:16:38 +0700

    Добавление методов push_front, pop_back, removeAt

--- lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
+++ lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
@@ -14,8 +14,12 @@ public:
 	void push_back(T data);
 	void clear();
 	int GetSize() { return Size; };
-
 	T& operator[](const int index);
+
+	void push_front(T data);
+	void pop_back();
+	void insert(T data, int index);
+	void removeAt(int index);
 
 private:
 	template<typename T>
@@ -97,6 +101,62 @@ T & List<T>::operator[](const int index)
 		}
 		current = current->pNext;
 		counter++;
+	}
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	head = new Node<T>(data,head); // сначала создается обьект, потом присваивается
+	Size++;
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	removeAt(Size - 1);
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+	if (index==0)
+	{
+		push_front(data);
+	}
+	else
+	{
+		Node<T> *previous = this->head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+		Node<T> *newNode = new Node<T>(data, previous->pNext);
+		previous->pNext = newNode;
+		Size++;
+	}
+}
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+	if (index == 0)
+	{
+		push_front();
+	}
+	else
+	{
+		Node<T> *previous = this->head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			previous = previous->pNext;
+		}
+		Node<T> *toDelete = previous->pNext;
+		previous->pNext = toDelete->pNext;
+		delete toDelete;
+		Size--;
 	}
 }
 
