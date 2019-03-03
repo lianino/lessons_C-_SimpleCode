19801700d081e336aed1aea068d71ff2bb8cd011
lianino <lianino@mail.ru>
2019-03-03 17:34:16 +0700

    Создание векторов, использование некоторых методов

--- lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
+++ lessons_CPP_SimpleCode/lessons_CPP_SimpleCode/main.cpp
@@ -1,192 +1,38 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-//односв€зный список
-
-template<typename T>
-class List
-{
-public:
-	List();
-	~List();
-
-	void pop_front();
-	void push_back(T data);
-	void clear();
-	int GetSize() { return Size; };
-	T& operator[](const int index);
-
-	void push_front(T data);
-	void pop_back();
-	void insert(T data, int index);
-	void removeAt(int index);
-
-private:
-	template<typename T>
-	class Node
-	{
-	public:
-		Node *pNext;
-		T data;
-		Node(T data = T(), Node *pNext = nullptr)
-		{
-			this->data = data;
-			this->pNext = pNext;
-		}
-	};
-	int Size;
-	Node<T> *head;
-};
-
-template<typename T>
-List<T>::List()
-{
-	Size = 0;
-	head = nullptr;
-}
-
-template<typename T>
-List<T>::~List()
-{
-	clear();
-}
-
-template<typename T>
-void List<T>::pop_front()
-{
-	Node<T> *temp = head;
-	head = head->pNext;
-	delete temp;
-	Size--;
-}
-
-template<typename T>
-void List<T>::push_back(T data)
-{
-	if (head == nullptr)
-	{
-		head = new Node<T>(data);
-	}
-	else
-	{
-		Node<T> *current = this->head;
-		while(current->pNext != nullptr)
-		{
-			current = current->pNext;
-		}
-		current->pNext = new Node<T>(data);
-	}
-	Size++;
-}
-
-template<typename T>
-void List<T>::clear()
-{
-	while (Size)
-	{
-		pop_front();
-	}
-}
-
-template<typename T>
-T & List<T>::operator[](const int index)
-{
-	int counter = 0;
-	Node<T> *current = this->head;
-	while (current != nullptr)
-	{
-		if (counter == index)
-		{
-			return current->data;
-		}
-		current = current->pNext;
-		counter++;
-	}
-}
-
-template<typename T>
-void List<T>::push_front(T data)
-{
-	head = new Node<T>(data,head); // сначала создаетс€ обьект, потом присваиваетс€
-	Size++;
-}
-
-template<typename T>
-void List<T>::pop_back()
-{
-	removeAt(Size - 1);
-}
-
-template<typename T>
-void List<T>::insert(T data, int index)
-{
-	if (index==0)
-	{
-		push_front(data);
-	}
-	else
-	{
-		Node<T> *previous = this->head;
-
-		for (int i = 0; i < index - 1; i++)
-		{
-			previous = previous->pNext;
-		}
-		Node<T> *newNode = new Node<T>(data, previous->pNext);
-		previous->pNext = newNode;
-		Size++;
-	}
-}
-
-template<typename T>
-void List<T>::removeAt(int index)
-{
-	if (index == 0)
-	{
-		push_front();
-	}
-	else
-	{
-		Node<T> *previous = this->head;
-
-		for (int i = 0; i < index - 1; i++)
-		{
-			previous = previous->pNext;
-		}
-		Node<T> *toDelete = previous->pNext;
-		previous->pNext = toDelete->pNext;
-		delete toDelete;
-		Size--;
-	}
-}
-
+//Ѕиблиотека стандартных шаблонов (STL)
+//VECTOR
 
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	List<int> lst;
-	lst.push_back(55);
-	lst.push_back(11);
-	lst.push_back(2);
+	vector<int> myVector = {67,45,78,90};
+	myVector.reserve(15);
+	myVector.push_back(2);
+	myVector.push_back(44);
+	myVector.push_back(77);
+	myVector.push_back(9);
 
-	for (int i = 0; i < lst.GetSize(); i++)
+	//cout << myVector.at(1) << endl;// доступ к элементу вектора с проверкой границы массива вотличии от использовани€ []
+
+	cout << " оличество элементов в векторе " << myVector.size() << endl;
+
+	cout << "Capasity вектора " << myVector.capacity() << endl;// покажет кол-во элементов с запасом
+
+	/*myVector.pop_back();
+
+	cout << " оличество элементов в векторе " << myVector.size() << endl;
+	
+	for (int i = 0; i < myVector.size(); i++)
 	{
-		cout << lst[i] << endl;
+		cout << myVector[i] << endl;
 	}
 
-	cout << endl << "Ёлементов в списке " << lst.GetSize() << endl << "выполн€ю метод clear" << endl << endl;
-
-	lst.clear();
-
-	for (int i = 0; i < lst.GetSize(); i++)
-	{
-		cout << lst[i] << endl;
-	}
-
-	cout << "Ёлементов в списке " << lst.GetSize() << endl;
-
+	myVector.clear();*/
 	system("pause");
 	return 0;
 }
